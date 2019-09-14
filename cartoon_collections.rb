f234039bd15bcd5002fb5f2cba389809f25c4366
Cody Fussell <cody.fussell@gmail.com>
2019-09-14 23:28:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index{|v, i| puts "#{i+1}. #{v}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map{|v| v.capitalize() + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call)
+  return call.any?{|x| x.length > 4}
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  new = cheeses.each_with_index{|ch, i| cheese_types[i] if cheeses.include?(ch)}
+  
 end
